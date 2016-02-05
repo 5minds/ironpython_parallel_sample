f29273ec3fb0d43d10a168dd456981bdea3642d3
Björn Röber <bjoern.roeber@5minds.de>
2016-02-05 18:16:52 +0100

    Checking working sample with AppDomains

---
+++ ironpython_parallel_sample/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ironpython_parallel_sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Elster.SmartMeter.Services.DeviceCommunication.PythonAPI;
+
+    using IronPython.Hosting;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Parallel.Invoke(ExecutScript, ExecutScript, ExecutScript, ExecutScript, ExecutScript);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ExecutScript()
+        {
+            var options = new Dictionary<string, object>();
+            options["Debug"] = false;
+
+            var pythonEngine = Python.CreateEngine(options);
+
+            var searchPaths = pythonEngine.GetSearchPaths();
+            searchPaths.Add(@"c:\dev\elster\ironpython_parallel_sample\ironpython_parallel_sample\Lib");  // IronPython Path
+            pythonEngine.SetSearchPaths(searchPaths);
+
+            var domain = CreateAppDomain();
+            var scriptExecutor = domain.CreateInstanceAndUnwrap(
+                    typeof(PythonScriptExecutor).Assembly.FullName,
+                    typeof(PythonScriptExecutor).FullName) as PythonScriptExecutor;
+
+            scriptExecutor.Execute();
+        }
+
+        private static AppDomain CreateAppDomain()
+        {
+            var domainSetup = new AppDomainSetup();
+            domainSetup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            var domain = AppDomain.CreateDomain(string.Empty, null, domainSetup);
+            return domain;
+        }
+    }
+}
---
+++ ironpython_parallel_sample/PythonScriptExecutor.cs
@@ -0,0 +1,45 @@
+﻿namespace Elster.SmartMeter.Services.DeviceCommunication.PythonAPI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    using IronPython.Hosting;
+
+    /// <summary>
+    /// Execute the DeviceCommunication python script with the required parameters.
+    /// </summary>
+    internal class PythonScriptExecutor : MarshalByRefObject
+    {
+        //private const string ResultVariableName = "RESULT";
+
+        public IReadOnlyDictionary<string, object> Execute()
+        {
+            var options = new Dictionary<string, object>();
+#if true
+            // Enable debugging from c# into python code
+            options["Debug"] = false;
+#endif
+            var pythonEngine = Python.CreateEngine(options);
+            var pythonScope = pythonEngine.CreateScope();
+
+            // set search paths
+            var searchPaths = pythonEngine.GetSearchPaths();
+            searchPaths.Add(@"c:\dev\elster\ironpython_parallel_sample\ironpython_parallel_sample\Lib");  // IronPython Path
+            pythonEngine.SetSearchPaths(searchPaths);
+
+            var executePath = @"c:\dev\elster\ironpython_parallel_sample\ironpython_parallel_sample\exec_dc_api.py";
+
+            // Calls DeviceCommunication PythonAPI execute file
+            var resultScope = pythonEngine.ExecuteFile(executePath, pythonScope);
+
+            return null;
+
+            // Get result dictionary
+            //var result = resultScope.GetVariable<Dictionary<string, object>>(ResultVariableName);
+
+            // Return readonly result dictionary
+            //return new ReadOnlyDictionary<string, object>(result);
+        }
+    }
+}
